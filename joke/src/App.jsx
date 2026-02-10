424bc281462abaa64e6927aae648273833ceff9e
Marcoz V. S. <marcozviniciussilva@gmail.com>
2026-02-10 10:45:27 -0300

    resolvido por bruno

--- joke/src/App.jsx
+++ joke/src/App.jsx
@@ -1,18 +1,18 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import TelaPiada from './pages/TelaPiada.jsx'
 
 
 function App() {
   return (
-  <Router>
-    <Routes>
-      <Route path= '/' element= {<TelaPiada/>}/>
-      <Route path= '/telaPiada' element= {<TelaPiada/>}/>
-      <Route path= '/filtro' element= {<TelaPiada/>}/>
-      <Route path= '/categories' element= {<TelaPiada/>}/>
-    </Routes>
-  </Router>
-  ) 
+    <Router>
+      <Routes>
+        <Route path='/' element={<TelaPiada />} />
+        <Route path='/telaPiada' element={<TelaPiada />} />
+        <Route path='/filtro' element={<TelaPiada />} />
+        <Route path='/categories' element={<TelaPiada />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
