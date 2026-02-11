5759fd4562feb965e8feb2b321f05d48e1bb2cba
RafaelFigueiredoCode <rafaelfigueira203@gmail.com>
2026-02-11 10:18:52 -0300

    mudanças no css

--- joke/src/pages/Home.jsx
+++ joke/src/pages/Home.jsx
@@ -5,17 +5,30 @@ export default function Home() {
     const navigate = useNavigate()
 
     return(
-        <div>
+        <div
+        style={{
+            display: 'grid',
+            placeItems: 'center',
+            height: '100vh',
+
+            gridTemplateRows: '170px 150px 150px 150px auto'
+
+            }}>
              <h1>Seu site de piadas preferido!!!</h1>
+<<<<<<< Updated upstream
              <div>
                  <h3>Selecione seu tipo de Piada Preferido e se divirta!</h3>
+=======
+             <div style={{textAlign: 'center'}}>
+                 <h3>Selecione sue tipo de Piada Preferido e se divirta!</h3>
+>>>>>>> Stashed changes
              <button onClick={() => navigate('/categories')} >Categorias</button>
              </div>
-             <div>
+             <div style={{textAlign: 'center'}}>
                 <h3>Piadas de qualquer tipo para Você</h3>
              <button onClick={() => navigate('/piadasAleatorias')} >Piadas Aleatórias</button>
              </div>
-             <div>
+             <div style={{textAlign: 'center'}}>
                  <h3>Apenas Piadas pra Maiores</h3>
              <button onClick={() => navigate('/piadaNFSW')} >Piadas +18</button>
             </div>
